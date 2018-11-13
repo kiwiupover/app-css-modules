2dc5441ba6f88f6b70138c014978b9ec1b556d67
Dave Laird <kiwiupover@gmail.com>
2018-11-12 20:49:45 -0800

    it is working

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,47 @@
 'use strict';
 
+const path = require('path');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+let cssModulesAddon = require.resolve('css-modules');
+
+let cssModulesAddonPath = path.join(cssModulesAddon, '..', 'app', 'styles');
+console.log('cssModulesAddonPath', cssModulesAddonPath);
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    // postcssOptions: {
+    //   compile: {
+    //     enabled: true, // defaults to true
+    //     browsers: ['last 3 versions'], // this will override config found in config/targets.js
+    //     // plugins: [
+    //     //   {
+    //     //     module: <module>,
+    //     //     options: {
+    //     //       ...
+    //     //     }
+    //     //   }
+    //     // ]
+    //   },
+    //   filter: {
+    //     enabled: true, // defaults to false
+    //     map: false, // defaults to inline, false in production
+    //     browsers: ['last 3 versions'], // this will override config found in config/targets.js
+    //     include: [
+    //       'styles/*.css',
+    //       cssModulesAddonPath
+    //     ],
+    //     // exclude: ['vendor/bootstrap/**/*'],
+    //     // plugins: [
+    //     //   {
+    //     //     module: <module>,
+    //     //     options: {
+    //     //       ...
+    //     //     }
+    //     //   }
+    //     // ]
+    //   }
+    // },
+
     cssModules: {
       // Emit a combined SCSS file for ember-cli-sass to compile
       intermediateOutputPath: 'app/styles/app.scss',
@@ -13,9 +51,34 @@ module.exports = function(defaults) {
 
       // Pass a custom parser/stringifyer through to PostCSS for processing modules
       postcssOptions: {
-        syntax: require('postcss-scss')
+        syntax: require('postcss-scss'),
+        filter: {
+          enabled: true, // defaults to false
+          map: false, // defaults to inline, false in production
+          browsers: ['last 3 versions'], // this will override config found in config/targets.js
+          include: [
+            'styles/*.css',
+            cssModulesAddonPath
+          ],
+          // exclude: ['vendor/bootstrap/**/*'],
+          // plugins: [
+          //   {
+          //     module: <module>,
+          //     options: {
+          //       ...
+          //     }
+          //   }
+          // ]
+        }
       }
-    }
+    },
+
+    sassOptions: {
+      extension: "scss",
+      includePaths: [
+        cssModulesAddonPath
+      ]
+    },
   });
 
   return app.toTree();
